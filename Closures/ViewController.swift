5de64eadf71d99f6efdedcc4d66e1ce5bada7c16
Saleh Enam Shohag <shohag402@gmail.com>
2019-01-19 18:04:14 +0600

    Initial Commit

---
+++ Closures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Closures
+//
+//  Created by Saleh Enam Shohag on 19/1/19.
+//  Copyright © 2019 Nazia Afroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
